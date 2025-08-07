371725fed9d406b22eb90f771678505d5d5e577b
raguram <raguram90@aol.com>
2025-08-07 18:25:16 +0100

    Configure serilog logger, hosting DI and run application

--- WebApp/ConsoleUI/Program.cs
+++ WebApp/ConsoleUI/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+var builder = new ConfigurationBuilder();
+BuildConfig(builder);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Build())
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+var host = Host.CreateDefaultBuilder(args)
+    .UseSerilog()
+    .ConfigureServices((context, services) =>
+    {
+    })
+    .Build();
+
+var svc = ActivatorUtilities.CreateInstance<ApplicationService>(host.Services);
+
+try
+{
+    svc.Start();
+}
+catch (Exception ex)
+{
+    Log.Logger.Fatal(ex, "Application failed to start.");
+    svc.Stop();
+    Log.CloseAndFlush();
+}
+
+static void BuildConfig(IConfigurationBuilder builder)
+{
+    builder.SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .AddEnvironmentVariables();
+}
