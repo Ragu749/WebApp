9b11a9032988bcc48e617c4abd0a74f8b7391063
raguram <raguram90@aol.com>
2025-08-11 23:14:03 +0100

    Add models for database

---
+++ WebApp/Domain/Player.cs
@@ -0,0 +1,10 @@
+﻿public class Player
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Surname { get; set; }
+    public required string Country { get; set; }
+    public int Age { get; set; }
+    public int TeamId { get; set; }
+    public required Team Team { get; set; }
+}
