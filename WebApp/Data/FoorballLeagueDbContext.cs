28610281e3a52401fad099e37b09d811f46fe717
raguram <raguram90@aol.com>
2025-08-11 23:13:54 +0100

    Add dbcontext to data project

---
+++ WebApp/Data/FoorballLeagueDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class FootballLeagueDbContext : DbContext
+{
+    public DbSet<League> Leagues { get; set; }
+    public DbSet<Team> Teams { get; set; }
+    public DbSet<Player> Players { get; set; }
+}
