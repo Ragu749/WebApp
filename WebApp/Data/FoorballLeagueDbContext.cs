ce76cfd23872780da8f24aec34b0481eeba3fb1e
raguram <raguram90@aol.com>
2025-10-21 08:25:42 +0100

    Add constructor to db context

--- WebApp/Data/FoorballLeagueDbContext.cs
+++ WebApp/Data/FoorballLeagueDbContext.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
-public class FootballLeagueDbContext : DbContext
+public class FootballLeagueDbContext: DbContext
 {
+    public FootballLeagueDbContext(DbContextOptions<FootballLeagueDbContext> options) : base(options)
+    {
+    }
+
     public DbSet<League> Leagues { get; set; }
     public DbSet<Team> Teams { get; set; }
     public DbSet<Player> Players { get; set; }
