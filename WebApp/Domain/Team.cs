8eea0251b006c9c33ad8f99af9129b0491bb0e3f
raguram <raguram90@aol.com>
2025-10-24 19:11:25 +0100

    Removed white line

--- WebApp/Domain/Team.cs
+++ WebApp/Domain/Team.cs
@@ -5,5 +5,5 @@
     public required string Country { get; set; }
     public int YearFounded { get; set; }
     public int LeagueId { get; set; }
-    public required League League { get; set; }
+    public required League League { get; set; }  
 }
